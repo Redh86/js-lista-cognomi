f2efc3ebb51c5cbbd0dd9f12bd018957ac71ebe6
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-02-25 10:43:00 +0100

    posizione "umana" chiarita e corretta in classe

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,12 +1,16 @@
 var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"]
 
 var cgnUtn = prompt ("Inserire cognome");
-console.log(cgnUtn);
 
 cognomi.push(cgnUtn);
 
 cognomi.sort();
-console.log(cognomi);
+
+console.log(cognomi)
 
 for (var i = 0; i < cognomi.length; i++)
-console.log(cognomi[i]);
+    if (cgnUtn == cognomi[i]) {
+        console.log("Posizione umana: " + (i + 1))
+    }
+
+
